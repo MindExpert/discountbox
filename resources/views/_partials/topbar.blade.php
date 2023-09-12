8d4be510e39cdc32f33d57180bf43b1679f82dd2
e-muchasaj <e.muchasaj@7btc.co>
2023-09-12 20:13:58 +0200

    lang comment

--- resources/views/_partials/topbar.blade.php
+++ resources/views/_partials/topbar.blade.php
@@ -28,33 +28,33 @@
         </div>
 
         <div class="d-flex">
-            <div class="dropdown d-inline-block">
-                @php($sessionLocale = session()->get('locale'))
-                <button type="button" class="btn header-item waves-effect"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    @switch($sessionLocale)
-                        @case('it')
-                            <img src="{{ asset('/assets/images/flags/it.jpg')}}" alt="Header Language" height="16">
-                            @break
-                        @default
-                            <img src="{{ asset('/assets/images/flags/en.jpg')}}" alt="Header Language" height="16">
-                    @endswitch
-                </button>
-                <div class="dropdown-menu dropdown-menu-end">
-                    @foreach(config('app.locales') as $locale)
-                        <form class="inline" action="{{ route('languages.update') }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <input type="hidden" name="locale" value="{{ $locale }}">
-                            <!-- item-->
-                            <button type="submit" class="dropdown-item notify-item language" data-lang="eng">
-                                <img src="{{ asset ("/assets/images/flags/$locale.jpg") }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">@lang("general.locales.{$locale}")</span>
-                            </button>
-                        </form>
-                    @endforeach
-                    <!-- item-->
-                </div>
-            </div>
+{{--            <div class="dropdown d-inline-block">--}}
+{{--                @php($sessionLocale = session()->get('locale'))--}}
+{{--                <button type="button" class="btn header-item waves-effect"--}}
+{{--                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                    @switch($sessionLocale)--}}
+{{--                        @case('it')--}}
+{{--                            <img src="{{ asset('/assets/images/flags/it.jpg')}}" alt="Header Language" height="16">--}}
+{{--                            @break--}}
+{{--                        @default--}}
+{{--                            <img src="{{ asset('/assets/images/flags/en.jpg')}}" alt="Header Language" height="16">--}}
+{{--                    @endswitch--}}
+{{--                </button>--}}
+{{--                <div class="dropdown-menu dropdown-menu-end">--}}
+{{--                    @foreach(config('app.locales') as $locale)--}}
+{{--                        <form class="inline" action="{{ route('languages.update') }}" method="POST">--}}
+{{--                            @csrf--}}
+{{--                            @method('PUT')--}}
+{{--                            <input type="hidden" name="locale" value="{{ $locale }}">--}}
+{{--                            <!-- item-->--}}
+{{--                            <button type="submit" class="dropdown-item notify-item language" data-lang="eng">--}}
+{{--                                <img src="{{ asset ("/assets/images/flags/$locale.jpg") }}" alt="user-image" class="me-1" height="12"> <span class="align-middle">@lang("general.locales.{$locale}")</span>--}}
+{{--                            </button>--}}
+{{--                        </form>--}}
+{{--                    @endforeach--}}
+{{--                    <!-- item-->--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
